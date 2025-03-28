aa0493b346d06d722e295186c321f077ea817438
prince <prince.oncada@gmail.com>
2025-03-28 18:44:29 -0400

    added preset colors in tailwind configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,14 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        "school-sky": "#C3EBFA",
+        "school-sky-light": "#EDF9FD",
+        "school-purple": "#CFCEFF",
+        "school-purple-light": "#F1F0FF",
+        "school-yellow": "#FAE27C",
+        "school-yellow-light": "#FEFCE8",
+      }
     },
   },
   plugins: [],
